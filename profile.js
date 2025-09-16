cfde28520f1dec5e51d260ac42f2e636c5c126f4
sosana waleed <sosanawaleed05@gmail.com>
2025-09-16 16:37:11 +0300

    sosanawaleed

---
+++ profile.js
@@ -0,0 +1,27 @@
+function addtask() {
+  const input = document.getElementById("taskinput");
+  const taskText = input.value.trim();
+
+  if (taskText === "") return;
+
+  const taskList = document.getElementById("taskList");
+
+  const li = document.createElement("li");
+  li.className = "task";
+  li.textContent = taskText;
+
+  li.addEventListener("click", () => {
+    li.classList.toggle("done");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.className = "delete-btn";
+    taskList.removeChild(li);
+  ;
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+ input.value = "";
+}
